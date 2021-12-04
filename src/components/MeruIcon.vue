f8f3adc1e6947156ef29a74479e62fd698dbf184
Azalea <noreply@aza.moe>
2021-12-04 15:23:45 -0500

    [+] Meruicon component

---
+++ src/components/MeruIcon.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="MeruIcon">
+        <img alt="meru" src="../../public/meru_256px.png">
+    </div>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+
+@Options({components: {}})
+export default class MeruIcon extends Vue
+{
+
+}
+</script>
+
+<style lang="sass" scoped>
+img
+    width: 160px
+    position: absolute
+    left: 0
+    bottom: 0
+</style>
