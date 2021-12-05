23c01d745c699d916811684a89be9b5fa5cd5875
Azalea <noreply@aza.moe>
2021-12-05 00:36:49 -0500

    [+] i18n

--- src/messages.ts
+++ src/messages.ts
@@ -2,13 +2,27 @@ import {createI18n} from "vue-i18n";
 
 export const messages = {
     en: {
-        
+        nav: {
+            home: 'Home',
+            about: 'About',
+            blog: 'Blog',
+            projects: 'Projects',
+        }
     },
     zh: {
-
+        // nav: {
+        //     home: '温馨家',
+        //     about: '关于我',
+        //     blog: '部落格',
+        //     projects: '我做的',
+        // }
     }
 }
 
 export const i18n = createI18n({
-    messages
+    messages,
+    locale: navigator.language.split('-')[0],
+    fallbackLocale: 'en'
 })
+
+console.log(navigator.language)
