d4947e07dd664c9e6d75149a81575ec82fb129cd
Hykilpikonna <me@hydev.org>
2021-12-05 01:26:00 -0500

    [+] Font awesome

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import {i18n} from "@/messages";
+import '@fortawesome/fontawesome-free/css/all.min.css'
 
 createApp(App).use(router).use(i18n).mount('#app')
