19a49d1eee61dbc011f0451dc0b7d7e3f862deb6
Azalea <noreply@aza.moe>
2021-12-07 00:26:55 -0500

    [+] New home page

---
+++ src/views/NewHome.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="NewHome">
+        <canvas id="three"></canvas>
+    </div>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+import {start} from "@/animation/home";
+
+@Options({components: {}})
+export default class NewHome extends Vue
+{
+    mounted(): void
+    {
+        start('three')
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+#three
+    width: 100vw
+    height: 100vh
+    position: absolute
+    top: 0
+    left: 0
+    z-index: -1
+</style>
