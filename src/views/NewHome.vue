b15d2c22701abc6d17389d00c77c256611d15bd8
Azalea <noreply@aza.moe>
2021-12-10 15:45:38 -0500

    [+] Editor controls

--- src/views/NewHome.vue
+++ src/views/NewHome.vue
@@ -1,16 +1,24 @@
 <template>
     <div id="NewHome">
         <canvas id="three"></canvas>
+
+        <!-- Editor controls -->
+        <div id="editor-controls" class="fbox-vcenter">
+            <span>Editor</span>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 import {Options, Vue} from 'vue-class-component';
 import {start} from "@/animation/Home";
+import {config} from "@/animation/Config";
 
 @Options({components: {}})
 export default class NewHome extends Vue
 {
+    editMode = config.editMode
+
     mounted(): void
     {
         start('three')
@@ -27,4 +35,18 @@ export default class NewHome extends Vue
     left: 0
     z-index: 1
     cursor: none
+
+#editor-controls
+    position: absolute
+    top: 20px
+    left: 0
+    z-index: 2
+
+    height: 50px
+    width: calc(100vw - 100px)
+    margin-left: 50px
+    border-radius: 50px
+
+    color: #ffeedb
+    background-image: linear-gradient(180deg, #000000 0%, #434343 100%)
 </style>
