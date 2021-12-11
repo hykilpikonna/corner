111419940a686ee9f41f71b57d873914d0ec747e
Hykilpikonna <me@hydev.org>
2021-12-11 12:19:46 -0500

    [O] Separate color picker test

---
+++ src/views/color/ColorPickerTest.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="Projects">
+        <ColorPicker v-model:color="color"></ColorPicker>
+        <button @click="log"></button>
+    </div>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+import MyColorPicker from "@/views/color/ColorPicker.vue";
+import {Color} from "three";
+
+@Options({components: {ColorPicker: MyColorPicker}})
+export default class Projects extends Vue
+{
+    color = new Color('#ffffff')
+
+    log(): void
+    {
+        console.log(this.color)
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
