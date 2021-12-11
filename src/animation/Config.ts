151673c599b83b35e6441c5a003b16b75c9a6711
Hykilpikonna <me@hydev.org>
2021-12-11 17:12:38 -0500

    [-] Remove unused configurations

--- src/animation/Config.ts
+++ src/animation/Config.ts
@@ -5,13 +5,8 @@ import {Color} from "three";
  */
 export const config = {
 
-    background: '#333',
-
     // Field of vision and cutoff frustum for near and far
     cam: {fov: 50, near: 1, far: 5000},
-
-    // Mouse movement factor
-    mouseFactor: 20,
 
     // Smooth movements (speeds are in terms of pixels per ms)
     smooth: {mouseSpeed: 10 * window.devicePixelRatio},
