8a48294f4e9115822f9cb1181c6d6dcd3269ec44
Hykilpikonna <me@hydev.org>
2021-12-25 17:07:50 -0500

    [+] Add blog view

---
+++ src/views/Blog.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="Blog">
+        
+    </div>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+
+@Options({components: {}})
+export default class Blog extends Vue
+{
+
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
