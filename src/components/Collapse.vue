0b5c9c82c57980ab6179351543580207ee5e6f94
Azalea <noreply@aza.moe>
2021-12-25 01:19:39 -0500

    [+] Add title to collapse

--- src/components/Collapse.vue
+++ src/components/Collapse.vue
@@ -1,20 +1,30 @@
 <template>
     <div class="collapse">
-        <slot></slot>
+        <h3 v-html="displayTitle"></h3>
+        <div class="content">
+            <slot></slot>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 import {Options, Vue} from 'vue-class-component';
+import {Prop} from "vue-property-decorator";
 
 @Options({components: {}})
 export default class Collapse extends Vue
 {
+    @Prop() title!: string
 
+    get displayTitle(): string
+    {
+        return decodeURIComponent(this.title)
+    }
 }
 </script>
 
 <style lang="sass">
 .collapse
-    display: none
+    .content
+        display: none
 </style>
