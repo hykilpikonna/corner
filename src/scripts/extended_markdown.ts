20365b9a7fb21dc140836823c0b9e55fe831795d
Azalea <noreply@aza.moe>
2021-12-25 00:00:04 -0500

    [+] Parse commands

--- src/scripts/extended_markdown.ts
+++ src/scripts/extended_markdown.ts
@@ -2,7 +2,7 @@
  * Regex used in markdown extension parsing
  */
 const re = {
-    command: /<!--{.*?}-->/g,
+    command: /<!--{.*?(?=}-->)/g,
     hashes: /^#+/g,
 }
 
@@ -16,6 +16,20 @@ function parseExtensions(raw: string): string
 {
     const lines = raw.replace('\r\n', '\n').split('\n')
     let i = 0
+
+    // Run all commands in markdown
+    while (i < lines.length)
+    {
+        // Find commands
+        const r = re.command.exec(lines[i])
+        if (!r) continue
+        const cmd = r[0].substring(5).trim()
+
+        // Run cmd
+        eval(cmd)
+
+        i++;
+    }
 
     return raw
 }
