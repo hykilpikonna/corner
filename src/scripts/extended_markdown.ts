9aa2c6de79618f8600b5bbec31e11a0d013ae2e0
Azalea <noreply@aza.moe>
2021-12-25 00:00:14 -0500

    [+] Markdown: hideSection() function

--- src/scripts/extended_markdown.ts
+++ src/scripts/extended_markdown.ts
@@ -17,6 +17,27 @@ function parseExtensions(raw: string): string
     const lines = raw.replace('\r\n', '\n').split('\n')
     let i = 0
 
+    /**
+     * Hide an entire hn section
+     * Example:
+     *
+     * ### Header3 <!--{ hideSection() }-->
+     *
+     * ... Hidden items
+     *
+     * ### Next Header3
+     */
+    function hideSection()
+    {
+        const r = re.hashes.exec(lines[i])
+        if (!r) return
+        const level = r[0].length
+
+        // Find next same-level or higher-level header's index
+        let j = i + 1
+        while (!(lines[i].startsWith('#') && re.hashes.exec(lines[i])![0].length <= level)) j++
+    }
+
     // Run all commands in markdown
     while (i < lines.length)
     {
