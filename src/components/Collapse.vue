5687839a61483c2cdf5abe53aeb115e582eed4d2
Azalea <noreply@aza.moe>
2021-12-25 00:48:09 -0500

    [+] Add collapse component

---
+++ src/components/Collapse.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="collapse">
+        <slot></slot>
+    </div>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+
+@Options({components: {}})
+export default class Collapse extends Vue
+{
+
+}
+</script>
+
+<style lang="sass">
+.collapse
+    display: none
+</style>
