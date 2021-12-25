0fee63972fc0f9348c0d8e0712e63df6895742b0
Hykilpikonna <me@hydev.org>
2021-12-25 13:25:11 -0500

    [+] Filter table

--- src/components/MetaTable.vue
+++ src/components/MetaTable.vue
@@ -18,9 +18,18 @@ export default class MetaTable extends Vue
 
     get filteredTable(): {[id: string]: unknown}
     {
-        const t = {...this.table}
-        for (let k in Object.keys(t))
-            if (!t[k]) delete t[k]
+        const t: {[id: string]: unknown} = {}
+
+        Object.keys(this.table).forEach(k => {
+            // Ignore empty
+            if (!this.table[k]) return
+
+            // Convert to sentence case (https://stackoverflow.com/a/7225450/7346633)
+            let newK = k.replace(/([A-Z])/g, " $1")
+            newK = newK.charAt(0).toUpperCase() + newK.slice(1)
+            t[newK] = this.table[k]
+        })
+
         return t
     }
 }
