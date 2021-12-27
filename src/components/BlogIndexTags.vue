cab155a2e4f0bbe6d185e53a020621280a630664
Azalea <noreply@aza.moe>
2021-12-27 12:20:06 -0500

    [+] BlogIndexTags.vue

---
+++ src/components/BlogIndexTags.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="index-tags">
+        <div v-for="t in meta.tags" :key="t">{{t}}</div>
+    </div>
+</template>
+
+<script lang="ts">
+import {Options, Vue} from 'vue-class-component';
+import {BlogMeta, staticMeta} from '@/views/Blog.vue'
+
+@Options({components: {}})
+export default class BlogIndexLinks extends Vue
+{
+    meta: BlogMeta = staticMeta
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
