ed6cf35be8a2296fbc665419d57561d3ddd41b69
Hykilpikonna <me@hydev.org>
2021-12-26 19:58:19 -0500

    [O] Easier production switch

--- src/scripts/constants.ts
+++ src/scripts/constants.ts
@@ -1,5 +1,6 @@
-// export const backendUrl = 'http://localhost:5000'
+const production = true
+
 export const hosts = {
     api: 'https://profile-api.hydev.org',
-    content: 'https://profile-content.hydev.org'
+    content: production ? 'https://profile-content.hydev.org' : 'http://localhost:8082'
 }
