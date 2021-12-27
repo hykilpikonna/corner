c063728a42410d96468a95241c95248b38311fc1
Hykilpikonna <me@hydev.org>
2021-12-27 12:37:39 -0500

    [+] BlogIndexCategories

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import {i18n} from "@/scripts/messages"
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import Collapse from "@/components/Collapse.vue"
 import BlogIndexTags from "@/components/BlogIndexTags.vue";
+import BlogIndexCategories from "@/components/BlogIndexCategories.vue";
 
 const app = createApp(App).use(router).use(i18n)
     .component('Collapse', Collapse)
     .component('BlogIndexTags', BlogIndexTags)
+    .component('BlogIndexCategories', BlogIndexCategories)
     .component('Dynamic', {
         props: ['template'],
         render() {
