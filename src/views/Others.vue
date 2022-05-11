dcc2aad6619ce8cc3a95e9164f8e2e0a72de5c6c
Azalea <noreply@aza.moe>
2022-05-11 13:37:41 -0400

    [F] Fix clicking outside the link

--- src/views/Others.vue
+++ src/views/Others.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="Others" class="general-page">
     <div class="title"><h2>更多链接</h2></div>
-    <router-link class="rlink" to="/kitchen-menu">🍳 小桂桂的私房菜 菜单</router-link>
-    <router-link class="rlink" to="/friends">🎎 朋友们</router-link>
+    <div class="links">
+      <router-link class="rlink" to="/kitchen-menu">🍳 小桂桂的私房菜 菜单</router-link>
+      <router-link class="rlink" to="/friends">🎎 朋友们</router-link>
+      <a href="http://status.hydev.org/">🔌 看看服务器们过得怎么样</a>
+    </div>
   </div>
 </template>
 
@@ -20,8 +23,12 @@ export default class Others extends Vue
 @import "src/css/colors"
 @import "src/css/responsive"
 
+.links
+  display: flex
+  flex-direction: column
+  align-items: flex-start
+
 a
   color: $color-text-main
-  display: block
   margin-bottom: 0.5em
 </style>
