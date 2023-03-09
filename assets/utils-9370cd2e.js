94831ae7212d0c862dfe96119696d1d7f5e9e98c
hykilpikonna <me@hydev.org>
2023-03-09 22:20:59 +0000

    Deploying to gh-pages from @ hykilpikonna/home-page@56ca0ab65520d63da91b70437411d273f34e6413 🚀

---
+++ assets/utils-9370cd2e.js
@@ -0,0 +1,1 @@
+function o(e){return e.charAt(0).toUpperCase()+e.slice(1)}function c(e){let t=e.length,n;for(;t!=0;)n=Math.floor(Math.random()*t),t--,[e[t],e[n]]=[e[n],e[t]];return e}export{o as c,c as s};
