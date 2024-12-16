ca2e9c3c620d3faf9a3e8d43637836545cdd37cb
Azalea <noreply@aza.moe>
2024-12-17 07:37:41 +0800

    [U] Update title

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="Home" class="fbox-center f-grow1">
         <div id="box">
-            <div class="font-script-en">Hykilpikonna's</div>
+            <div class="font-script-en">Azalea's</div>
             <div class="font-script-en bold">Road Less Traveled</div>
         </div>
     </div>
